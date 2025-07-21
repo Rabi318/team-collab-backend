53844c9da8096f7620195b85aaae77f42ea56b09
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-07-21 11:52:30 +0530

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://euphonious-duckanoo-508032.netlify.app",
     credentials: true,
   })
 );
@@ -44,7 +44,7 @@ app.use((req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://euphonious-duckanoo-508032.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
